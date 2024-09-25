6886c156a9818147208732db54d436507f7f727c
EllaLiu0401 <ellatong0515@gmail.com>
2024-09-25 23:36:48 +1000

    feat: restart a quiz

--- src/components/App.js
+++ src/components/App.js
@@ -44,6 +44,12 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+      };
     default:
       throw new Error("Action unknown");
   }
@@ -104,6 +110,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
